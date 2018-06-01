594f0ffa38821bfce69ac92c79c8db33554b1d0a
Andrew Barclay <andrewkbarclay@gmail.com>
2018-06-01 19:01:34 +0000

    added outer rim line

--- client/js/PolarDiagram.js
+++ client/js/PolarDiagram.js
@@ -12,7 +12,9 @@ export default class PolarDiagram {
         displayHeight: options.displayHeight || 800,
         margin: {top: 20, right: 20, bottom: 20, left: 20},
         radialPaddingFactor: 1.1,
-        minDegreesToWind: options.minDegreesToWind || 30
+        minDegreesToWind: options.minDegreesToWind || 30,
+        outerRimThickness: 1
+
       };
 
 
@@ -20,6 +22,11 @@ export default class PolarDiagram {
     this._radialScale = d3.scaleLinear()
       .range([0, this._config.displayWidth - this._config.margin.left - this._config.margin.right])
       .domain([0, this.maxValue * this._config.radialPaddingFactor]);
+
+    // maps degrees from wind to applicable angle for display (in radians)
+    this._angularScale = d3.scaleLinear()
+      .range([Math.radians(this._config.minDegreesToWind), Math.PI])
+      .domain([this._config.minDegreesToWind, 180]);
 
     this.baseGroup = this._drawSvgContainer(container);
     this.mainAxis = this._drawMainAxis(this.baseGroup);
@@ -43,114 +50,66 @@ export default class PolarDiagram {
     // Create group to hold axis and main radial gridlines
     const axisGroup = parentGroup.append("g").attr("class", "axisGroup");
 
-    const radialGridline = axisGroup.selectAll(".radialGridline")
-          .data(this._radialGridlineData)
-          .enter().append("g")
-            .attr("class", ".radialGridline");
-        //Append the lines
-        radialGridline.append("line")
-          .attr("x1", 0)
-          .attr("y1", 0)
-          .attr("x2", (d, i) => { return this._radialScale(d.cart()[0]) })
-          .attr("y2", (d, i) => { return this._radialScale(d.cart()[1]) })
-          .attr("class", "line")
-          .style("stroke", "black")
-          .style("stroke-width", "2px");
+    // draw main radial gridlines at minDegreesToWind, 90deg, 180 deg
+    axisGroup.selectAll(".radialGridline")
+      .data(this._radialGridlineData)
+      .enter().append("g")
+        .attr("class", ".radialGridline")
+      .append("line")
+        .attr("x1", 0)
+        .attr("y1", 0)
+        .attr("x2", (d, i) => { return this._radialScale(d.cart()[0]) })
+        .attr("y2", (d, i) => { return this._radialScale(d.cart()[1]) })
+        .attr("class", "line")
+        .style("stroke", "black")
+        .style("stroke-width", "2px");
 
+    // draw outer rim
+    const dOuterRadius = d3.arc()
+      .innerRadius(this._radialScale(this._paddedOuterRadius) - this._config.outerRimThickness)
+      .outerRadius(this._radialScale(this._paddedOuterRadius))
+      .startAngle(this._angularScale(this._config.minDegreesToWind))
+      .endAngle(Math.PI);
+
+    axisGroup.append("path")
+      .attr("d", dOuterRadius());
+
+    // draw main scale for boat speed
     axisGroup.append("g")
-      .call(d3.axisBottom(this._radialScale));
+      .call(d3.axisTop(this._radialScale));
     return axisGroup;
   }
 
+  // return array of Coordinate objects corresponding to outer ends of radial gridlines.
   get _radialGridlineData(){
     const angles = [this._config.minDegreesToWind, 90, 180];
 
     return angles.map((angle) => {
-      const coord =  Coordinate.polar({
-        coords: [this.maxValue * this._config.radialPaddingFactor, angle - 90],
+      return  Coordinate.polar({
+        coords: [this._paddedOuterRadius, angle - 90],
         isDegree: true
       });
-      console.log(coord.cart());
-      return coord;
     })
 
+  }
+
+  get _paddedOuterRadius(){
+    return this.maxValue * this._config.radialPaddingFactor;
   }
 
   get maxValue() {
     return 10;
   }
 
-
 }
 
+// Angular Helper Functions
+// Converts from degrees to radians.
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
 
-// function GeneratePolarChartOld(container, data) {
-//
-//   var cfg = {
-//    w: 800,				//Width of the circle
-//    h: 800,				//Height of the circle
-//    margin: {top: 20, right: 20, bottom: 20, left: 20}, //The margins of the SVG
-//    levels: 3,				//How many levels or inner circles should there be drawn
-//    maxValue: 10, 			//What is the value that the biggest circle will represent
-//    labelFactor: 0.9, 	//How much farther than the radius of the outer circle should the labels be placed
-//    wrapWidth: 60, 		//The number of pixels after which a label needs to be given a new line
-//    opacityArea: 0.35, 	//The opacity of the area of the blob
-//    dotRadius: 4, 			//The size of the colored circles of each blog
-//    opacityCircles: 0.1, 	//The opacity of the circles of each blob
-//    strokeWidth: 2, 		//The width of the stroke around each blob
-//    roundStrokes: false,	//If true the area and stroke will follow a round path (cardinal-closed)
-//    color: d3.scaleOrdinal(d3.schemeCategory10)	//Color function
-//   };
-//
-//   let maxValue = cfg.maxValue;
-//
-//   //Scale for the radius
-//
-//   let radius = Math.min(cfg.w/2, cfg.h/2);
-//
-//   const rScale = d3.scaleLinear()
-//     .range([0, radius])
-//     .domain([0, maxValue]);
-//
-//   //Initiate the radar chart SVG
-//   const svg = d3.select(container).append("svg")
-//       .attr("width",  cfg.w + cfg.margin.left + cfg.margin.right)
-//       .attr("height", cfg.h + cfg.margin.top + cfg.margin.bottom)
-//   //Append a g element
-//   const g = svg.append("g")
-//       .attr("transform", "translate(" + (cfg.w/2 + cfg.margin.left) + "," + (cfg.h/2 + cfg.margin.top) + ")");
-//
-//
-//   const addLines = function(minAngle){
-//     const axisGrid = g.append("g").attr("class", "axisWrapper");
-//
-//     const axis = axisGrid.selectAll(".axis")
-//       .data(["A", "B", "C", "D"])
-//       .enter().append("g")
-//         .attr("class", "axis");
-//     //Append the lines
-//     axis.append("line")
-//       .attr("x1", 0)
-//       .attr("y1", 0)
-//       .attr("x2", function(d, i){ return rScale(maxValue*1.1) * Math.cos(1*(i + 1) - Math.PI/2); })
-//       .attr("y2", function(d, i){ return rScale(maxValue*1.1) * Math.sin(1*(i + 1) - Math.PI/2); })
-//       .attr("class", "line")
-//       .style("stroke", "white")
-//       .style("stroke-width", "2px");
-//
-//     //Append the labels at each axis
-//     axis.append("text")
-//       .attr("class", "legend")
-//       .style("font-size", "11px")
-//       .attr("text-anchor", "middle")
-//       .attr("dy", "0.35em")
-//       .attr("x", function(d, i){ return rScale(maxValue * cfg.labelFactor) * Math.cos(1*(i + 1) - Math.PI/2); })
-//       .attr("y", function(d, i){ return rScale(maxValue * cfg.labelFactor) * Math.sin(1*(i + 1) - Math.PI/2); })
-//       .text(function(d){return d});
-//
-//     axisGrid.append("g")
-//       .call(d3.axisBottom(rScale));
-//   };
-//
-//   addLines();
-// }
+// Converts from radians to degrees.
+Math.degrees = function(radians) {
+  return radians * 180 / Math.PI;
+};
